22e632b031a9d091469754f17137fd5ede75f262
RimmaBuzuluk <rimmysya2000@gmail.com>
2023-08-08 20:25:44 +0300

    nodeJS

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
-const express=require('express')
+import express from "express"
 
-const PORT =process.env.PORT || 8080
+const app =express();
 
-const app =express()
+app.use(express.json())
 
-app.get('/',(req,res)=>{
-    res.send("hello")
+app.get('/',(req, res)=>{
+    res.send('hello world')
 })
 
-app.listen(PORT, ()=>console.log(`server started on port ${PORT}`))
+app.post('/auth/login',(req,res)=>{
+    console.log(req.body)
+    res.json({
+        success:true
+    });
+})
+
+app.listen(4444, (err)=>{
+    if(err){
+        return console.log(err)
+    }
+    console.log('server ok')
+}) ;
