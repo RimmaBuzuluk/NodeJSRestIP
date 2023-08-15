be71d15b9eca3664df1f412eeb767f5b5fa456fd
RimmaBuzuluk <rimmysya2000@gmail.com>
2023-08-15 22:03:03 +0300

    post get all

--- index.js
+++ index.js
@@ -21,9 +21,12 @@ app.use(express.json())
 
 app.post('/auth/login', loginValidation,UserController.login);
 app.post('/auth/register',registerValidation, UserController.register)
-app.get('/auth/me', checkAuth,UserController.getMe)
+app.get('/auth/me', checkAuth, UserController.getMe)
 
+
+app.get('/posts', PostController.getAll);
 app.post('/posts', checkAuth, postCreateValidation,PostController.create);
+
 
 app.listen(4444, (err)=>{
     if(err){
